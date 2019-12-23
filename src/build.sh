582a444d5daccebfc989c9cd47f9b1f0220da099
Antoni Alvarez <t.alvarezpascual@gmail.com>
2019-12-23 19:21:38 +0100

    Script Bash para ejecutar JFlex y CUP

---
+++ src/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Ejecutando JFlex"
+
+if java -jar lib/jflex-full-1.7.0.jar compiler/lexic/Scanner.flex; then
+  echo "JFlex ejecutado correctamente."
+
+  cd compiler/sintax
+
+  if java -jar lib/java-cup-11b.jar Parser.cup; then
+    echo "CUP ejecutado correctamente"
+  else
+    echo "Error de CUP"
+  fi
+else
+  echo "Error de JFlex"
+fi
