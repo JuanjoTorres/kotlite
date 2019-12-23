fd8c41cc9468c4825e20472cb2ca083e64971786
Antoni Alvarez <t.alvarezpascual@gmail.com>
2019-12-23 19:24:44 +0100

    Añadir comentarios

--- src/build.sh
+++ src/build.sh
@@ -7,6 +7,8 @@ if java -jar lib/jflex-full-1.7.0.jar compiler/lexic/Scanner.flex; then
 
   cd compiler/sintax
 
+  echo "Ejecutando Java CUP"
+
   if java -jar ../../lib/java-cup-11b.jar Parser.cup; then
     echo "CUP ejecutado correctamente"
   else
