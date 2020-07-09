5516c4bf3c002e9678c692485dec6f2abdc57f39
Juanjo Torres <jjtr96@gmail.com>
2020-07-09 19:22:28 +0200

    Clase enum actualizada.

--- src/compiler/codegeneration/Operation.java
+++ src/compiler/codegeneration/Operation.java
@@ -1,10 +1,11 @@
 package compiler.codegeneration;
 
 public enum Operation {
-    ADD, SUBS, PROD, DIV,
-    ASSIG,
-    EQU, NOTEQU, GT, LT, GTEQU, LTQUE,
-    NEG, AND, OR, NOT,
-    SKIP, GOTO, IFGOTO,
-    END, CALL, RETURN
+    ADD, SUB, PROD, DIV, NEG,           // Operaciones aritmeticas
+    COPY,                               // Operacion de asignacion
+    EQU, NOTEQU, GT, LT, GTEQU, LTQUE,  // Operaciones relacionales
+    AND, OR, NOT,
+    SKIP, GOTO,                         // Operaciones de saltos incondicionales
+    IFEQ, IFNE, IFGT, IFLT, IFGE, IFLE, // Operaciones de saltos condicionales
+    PMB, CALL, PARAM, RTN               // Operaciones de subprogramas
 }
